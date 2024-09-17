7d83e5f2c4b7fa6a688654aab34b83b4398df14a
janeny117 <jsja031017@kookmin.ac.kr>
2024-09-17 13:23:47 +0900

    Feat: 로그인 페이지 생성 - 로고 삽입 - 텍스트 필드 컴포넌트 생성 - 버튼 컴포넌트 생성 - 임시 로그인 버튼 삽입 (네이버, 구글, 카카오)

---
+++ lib/theme/light_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.white,
+    primary: Color(0xFFFF8200),
+    secondary: Color(0xFF002E5D),
+    outline:  Color(0xFFD9D9D9)
+  )
+);
