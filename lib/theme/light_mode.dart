ca24d6edba50295cca7e1056eb5bc7c7f4d16a8e
TaeYoon <tyk117@kookmin.ac.kr>
2024-10-26 14:14:22 +0900

    Fix: const error

--- lib/theme/light_mode.dart
+++ lib/theme/light_mode.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
-  colorScheme: ColorScheme.light(
-    background: Colors.white,
-    primary: Color(0xFFFF8200),
-    secondary: Color(0xFF002E5D),
-    outline:  Color(0xFFD9D9D9)
-  )
-);
+    colorScheme: const ColorScheme.light(
+        background: Colors.white,
+        primary: Color(0xFFFF8200),
+        secondary: Color(0xFF002E5D),
+        outline: Color(0xFFD9D9D9)));
