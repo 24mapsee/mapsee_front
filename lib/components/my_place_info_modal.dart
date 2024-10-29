d3d7b675513af0a1d53a29df2835a30d430e9f89
janeny117 <jsja031017@kookmin.ac.kr>
2024-10-29 18:31:58 +0900

    Feat: 장소 이름 줄바꿈 추가

--- lib/components/my_place_info_modal.dart
+++ lib/components/my_place_info_modal.dart
@@ -78,7 +78,6 @@ class _MyPlaceInfoModalState extends State<MyPlaceInfoModal> {
                   ),
                 ),
               ),
-              // Scrollable Content
               Expanded(
                 child: ListView(
                   controller: scrollController,
@@ -86,10 +85,12 @@ class _MyPlaceInfoModalState extends State<MyPlaceInfoModal> {
                     Container(
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(
-                                  color:
-                                      Theme.of(context).colorScheme.outline))),
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Theme.of(context).colorScheme.outline,
+                          ),
+                        ),
+                      ),
                       width: screenWidth,
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -100,39 +101,43 @@ class _MyPlaceInfoModalState extends State<MyPlaceInfoModal> {
                             children: [
                               Flexible(
                                 child: Text(
-                                  widget.title,
+                                  widget.title.replaceFirst(' ', '\n'),
                                   style: TextStyle(
-                                      fontWeight: FontWeight.w200,
-                                      fontSize: 30,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onSurface,
-                                      decoration: TextDecoration.none),
-                                  maxLines: 1,
+                                    fontWeight: FontWeight.w200,
+                                    fontSize: 30,
+                                    color: Theme.of(context).colorScheme.onSurface,
+                                    decoration: TextDecoration.none,
+                                  ),
+                                  textAlign: TextAlign.center,
                                   softWrap: true,
-                                  overflow: TextOverflow.ellipsis,
                                 ),
                               ),
+
+
                             ],
                           ),
                           SizedBox(width: 8),
-                          Text(widget.category.split('>').last,
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.black45,
-                                  decoration: TextDecoration.none),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis),
+                          Text(
+                            widget.category.split('>').last,
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.black45,
+                              decoration: TextDecoration.none,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
                           SizedBox(height: 20),
                           MyDepartureAndArrivalButtons(),
                           SizedBox(height: 15),
                           Container(
                             decoration: BoxDecoration(
-                                border: Border(
-                                    top: BorderSide(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .outline))),
+                              border: Border(
+                                top: BorderSide(
+                                  color: Theme.of(context).colorScheme.outline,
+                                ),
+                              ),
+                            ),
                             padding: EdgeInsets.only(top: 10),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -140,9 +145,7 @@ class _MyPlaceInfoModalState extends State<MyPlaceInfoModal> {
                                 Column(
                                   children: [
                                     GestureDetector(
-                                      onTap: () {
-                                        savePlace();
-                                      },
+                                      onTap: savePlace,
                                       child: Image.asset(
                                         isSaved
                                             ? 'assets/images/png/filled_heart.png'
@@ -156,12 +159,13 @@ class _MyPlaceInfoModalState extends State<MyPlaceInfoModal> {
                                     Text(
                                       '저장하기',
                                       style: TextStyle(
-                                          fontSize: 15,
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .secondary,
-                                          decoration: TextDecoration.none),
-                                    )
+                                        fontSize: 15,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                        decoration: TextDecoration.none,
+                                      ),
+                                    ),
                                   ],
                                 ),
                                 MyVerticalDivider(height: 0.05),
@@ -176,11 +180,12 @@ class _MyPlaceInfoModalState extends State<MyPlaceInfoModal> {
                                     ),
                                     Text('공유하기',
                                         style: TextStyle(
-                                            fontSize: 15,
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .secondary,
-                                            decoration: TextDecoration.none))
+                                          fontSize: 15,
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .secondary,
+                                          decoration: TextDecoration.none,
+                                        ))
                                   ],
                                 )
                               ],
@@ -367,5 +372,3 @@ class _MyPlaceInfoModalState extends State<MyPlaceInfoModal> {
     );
   }
 }
-
-
