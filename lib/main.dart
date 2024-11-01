c7ccd0dc808900d8819a61c6c66a578df16a23f7
TaeYoon <tyk117@kookmin.ac.kr>
2024-11-02 01:11:19 +0900

    Fix: version chk

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,18 @@ void main() async {
       onAuthFailed: (ex) {
         log("********* 네이버맵 인증오류 : $ex *********");
       });
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
