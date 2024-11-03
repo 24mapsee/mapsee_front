332841f6813e470f332fe08f7fe638b4a3841943
TaeYoon <tyk117@kookmin.ac.kr>
2024-11-03 21:03:09 +0900

    Fix: profile edit

--- lib/pages/following_follower_page.dart
+++ lib/pages/following_follower_page.dart
@@ -6,13 +6,12 @@ import 'package:http/http.dart' as http;
 import '../utils/common.dart';
 import 'package:mapsee/pages/external_profile_page.dart';
 
-
 class FollowingFollowerPage extends StatefulWidget {
   final int initialTabIndex;
   final String accessUserId;
-  
 
-  const FollowingFollowerPage({super.key, required this.initialTabIndex,required this.accessUserId});
+  const FollowingFollowerPage(
+      {super.key, required this.initialTabIndex, required this.accessUserId});
 
   @override
   State<FollowingFollowerPage> createState() => _FollowingFollowerPageState();
@@ -29,106 +28,106 @@ class _FollowingFollowerPageState extends State<FollowingFollowerPage>
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 2, vsync: this, initialIndex: widget.initialTabIndex);
-      // Fetch loginUserId asynchronously
-  getUserId().then((id) {
-    setState(() {
-      loginUserId = id;
+    _tabController = TabController(
+        length: 2, vsync: this, initialIndex: widget.initialTabIndex);
+    // Fetch loginUserId asynchronously
+    getUserId().then((id) {
+      setState(() {
+        loginUserId = id;
+      });
     });
-  });
     print("accessUserId: ${widget.accessUserId}");
     fetchFollowerAndFollowingData();
   }
-   Future<void> fetchFollowerAndFollowingData() async {
 
-  final followerUrl = '${dotenv.env["API_BASE_URL"]}/follow/follower/${widget.accessUserId}';
-  final followingUrl = '${dotenv.env["API_BASE_URL"]}/follow/following/${widget.accessUserId}';
-  
+  Future<void> fetchFollowerAndFollowingData() async {
+    final followerUrl =
+        '${dotenv.env["API_BASE_URL"]}/follow/follower/${widget.accessUserId}';
+    final followingUrl =
+        '${dotenv.env["API_BASE_URL"]}/follow/following/${widget.accessUserId}';
 
-  try {
-    final followerResponse = await http.get(Uri.parse(followerUrl));
-    final followingResponse = await http.get(Uri.parse(followingUrl));
-    
+    try {
+      final followerResponse = await http.get(Uri.parse(followerUrl));
+      final followingResponse = await http.get(Uri.parse(followingUrl));
 
-    if (followerResponse.statusCode == 200 && followingResponse.statusCode == 200) {
-      final followerJson = json.decode(followerResponse.body);
-      final followingJson = json.decode(followingResponse.body);
+      if (followerResponse.statusCode == 200 &&
+          followingResponse.statusCode == 200) {
+        final followerJson = json.decode(followerResponse.body);
+        final followingJson = json.decode(followingResponse.body);
 
-      setState(() {
-        // 중첩된 followers와 following 리스트를 추출하여 할당
-        followerData = List<Map<String, dynamic>>.from(followerJson['followers']);
-        followingData = List<Map<String, dynamic>>.from(followingJson['following']);
-        isLoading = false;
-      });
+        setState(() {
+          // 중첩된 followers와 following 리스트를 추출하여 할당
+          followerData =
+              List<Map<String, dynamic>>.from(followerJson['followers']);
+          followingData =
+              List<Map<String, dynamic>>.from(followingJson['following']);
+          isLoading = false;
+        });
 
-      print("Follower Data: $followerData");
-      print("Following Data: $followingData");
-    } else {
-      print('Failed to load data');
+        print("Follower Data: $followerData");
+        print("Following Data: $followingData");
+      } else {
+        print('Failed to load data');
+      }
+    } catch (e) {
+      print('Error fetching data: $e');
     }
-  } catch (e) {
-    print('Error fetching data: $e');
   }
-}
+
 // 팔로우 삭제 함수
- Future<void> deleteFollower(String userId) async {
-  final url = '${dotenv.env["API_BASE_URL"]}/follow/delete-follow';
-  print("요청 URL: $url");
-  try {
-    final response = await http.delete(
-      Uri.parse(url),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'followerId': userId,
-        'followingId': widget.accessUserId,
-      }),
-    );
+  Future<void> deleteFollower(String userId) async {
+    final url = '${dotenv.env["API_BASE_URL"]}/follow/delete-follow';
+    print("요청 URL: $url");
+    try {
+      final response = await http.delete(
+        Uri.parse(url),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'followerId': userId,
+          'followingId': widget.accessUserId,
+        }),
+      );
 
-    if (response.statusCode == 200 || response.statusCode == 204) {
-      print('Successfully unfollowed user: $userId');
-      fetchFollowerAndFollowingData();  // 업데이트된 팔로잉 데이터 가져오기
-    } else {
-      print('Failed to unfollow user: ${response.statusCode}');
+      if (response.statusCode == 200 || response.statusCode == 204) {
+        print('Successfully unfollowed user: $userId');
+        fetchFollowerAndFollowingData(); // 업데이트된 팔로잉 데이터 가져오기
+      } else {
+        print('Failed to unfollow user: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error unfollowing user: $e');
     }
-  } catch (e) {
-    print('Error unfollowing user: $e');
   }
-}
-
 
 // 팔로우 요청 보내는 함수
   Future<void> doFollowing(String userId) async {
-  final url = '${dotenv.env["API_BASE_URL"]}/follow/add-follow';
-  print("요청 URL: $url");
-  try {
-    final loginUserId = await getUserId();
-    if (loginUserId == null) {
-    print('로그인된 사용자가 없습니다.');
-    return;
-  }
-    final response = await http.post(
-      Uri.parse(url),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'followerId': userId,
-        'followingId': loginUserId,
-      }),
-    );
+    final url = '${dotenv.env["API_BASE_URL"]}/follow/add-follow';
+    print("요청 URL: $url");
+    try {
+      final loginUserId = await getUserId();
+      if (loginUserId == null) {
+        print('로그인된 사용자가 없습니다.');
+        return;
+      }
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'followerId': userId,
+          'followingId': loginUserId,
+        }),
+      );
 
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      print('Successfully followed user: $userId');
-      fetchFollowerAndFollowingData();  // 업데이트된 팔로잉 데이터 가져오기
-    } else {
-      print('Failed to follow user: ${response.statusCode}');
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        print('Successfully followed user: $userId');
+        fetchFollowerAndFollowingData(); // 업데이트된 팔로잉 데이터 가져오기
+      } else {
+        print('Failed to follow user: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error following user: $e');
     }
-  } catch (e) {
-    print('Error following user: $e');
   }
-}
-
-
-
-
 
   @override
   void dispose() {
@@ -165,7 +164,6 @@ class _FollowingFollowerPageState extends State<FollowingFollowerPage>
                       style: const TextStyle(
                         fontSize: 15,
                       ),
-
                     ),
                     const Text(
                       '명',
@@ -187,7 +185,6 @@ class _FollowingFollowerPageState extends State<FollowingFollowerPage>
                       style: const TextStyle(
                         fontSize: 15,
                       ),
-
                     ),
                     const Text(
                       '명',
@@ -202,16 +199,15 @@ class _FollowingFollowerPageState extends State<FollowingFollowerPage>
             child: TabBarView(
               controller: _tabController,
               children: [
-              _buildListView(
-                context,followerData,"팔로잉", doFollowing
-              ),
-              _buildListView(
-                context, 
-                followingData, 
-                (loginUserId == widget.accessUserId) ? "삭제" : "팔로잉", 
-                (loginUserId == widget.accessUserId) ? deleteFollower : doFollowing
-              ),
-            ],
+                _buildListView(context, followerData, "팔로잉", doFollowing),
+                _buildListView(
+                    context,
+                    followingData,
+                    (loginUserId == widget.accessUserId) ? "삭제" : "팔로잉",
+                    (loginUserId == widget.accessUserId)
+                        ? deleteFollower
+                        : doFollowing),
+              ],
             ),
           ),
         ],
@@ -221,7 +217,8 @@ class _FollowingFollowerPageState extends State<FollowingFollowerPage>
 }
 
 Widget _buildListView(BuildContext context, List<Map<String, dynamic>> data,
-    String buttonText, Future<void> Function(String) onButtonPressed) { // 1. onButtonPressed 타입 수정
+    String buttonText, Future<void> Function(String) onButtonPressed) {
+  // 1. onButtonPressed 타입 수정
   final double screenWidth = MediaQuery.of(context).size.width;
 
   print("Data in _buildListView: $data"); // 리스트 데이터 확인
@@ -237,21 +234,23 @@ Widget _buildListView(BuildContext context, List<Map<String, dynamic>> data,
 
   return ListView.builder(
     itemCount: data.length,
-     itemBuilder: (context, index) {
+    itemBuilder: (context, index) {
       final userName = data[index]['name'] ?? 'Unknown User'; // 기본값 설정
       final userId = data[index]['user_id'] ?? 'Unknown ID'; // 기본값 설정
-      final profileImg = data[index]['profile_picture'] ?? 'assets/images/dummy/default_user.png';
+      final profileImg = data[index]['profile_picture'] ?? '';
 
       print("Rendering item: $userName, $userId"); // 각 아이템 데이터 확인
 
       return ListTile(
-         leading: profileImg.startsWith('http')
-            ? Image.network(profileImg, width: 40, height: 40)  // URL인 경우 Image.network 사용
-            : Image.asset(profileImg, width: 40, height: 40),   // 로컬 파일인 경우 Image.asset 사용
+        leading: profileImg.startsWith('http')
+            ? Image.network(profileImg,
+                width: 40, height: 40) // URL인 경우 Image.network 사용
+            : Image.asset(profileImg,
+                width: 40, height: 40), // 로컬 파일인 경우 Image.asset 사용
 
         title: Text(userName),
         trailing: ElevatedButton(
-          onPressed: () =>onButtonPressed(userId),
+          onPressed: () => onButtonPressed(userId),
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.grey[200],
             foregroundColor: Colors.black,
@@ -269,4 +268,3 @@ Widget _buildListView(BuildContext context, List<Map<String, dynamic>> data,
     },
   );
 }
-
